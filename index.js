15b44c52556391404d899fadce6874128ae53da3
pythonlearner1025 <mjsong2021@gmail.com>
2024-05-10 19:06:13 -0700

    add favicon

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-fetch('/navbar.html')
-    .then(response => response.text())
-    .then(html => {
-    document.getElementById('navbar').innerHTML = html;
-});
+fetch('/navbar.html') 
+  .then(response => response.text())
+  .then(html => {
+    const navbarEl = document.getElementById('navbar');
+    navbarEl.innerHTML = html;
+
+    // Insert the favicon link tag
+    const link = document.createElement('link');
+    link.rel = 'shortcut icon';
+    link.href = '/assets/profile.jpeg';
+    document.head.appendChild(link);
+  });
